3a9e899961bc026c7b4d6a863215d49c98ec4ea6
saska318 <tsaska98@gmail.com>
2026-02-24 21:01:40 +0100

    chore: initialize Angular project and set up base project structure

---
+++ fitness-tracker-ui/src/app/features/workout/workout-form/workout-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-form',
+  imports: [],
+  templateUrl: './workout-form.html',
+  styleUrl: './workout-form.css',
+})
+export class WorkoutForm {
+
+}
